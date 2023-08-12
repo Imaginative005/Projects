95199760dcf7ebbd9d0faa22e34793e039946abd
Onyebuchi Iyase <117869405+Imaginative005@users.noreply.github.com>
2023-08-12 13:42:59 -0400

    Add files via upload

--- main.py
+++ main.py
@@ -1,60 +1,31 @@
-from selenium import webdriver
-from selenium.webdriver.common.keys import Keys
-from selenium.common.exceptions import ElementClickInterceptedException, NoSuchElementException
-from time import sleep
+from flask import Flask, render_template
+from post import Post
+import requests
 
-FB_EMAIL = YOUR FACEBOOK LOGIN EMAIL
-FB_PASSWORD = YOUR FACEBOOK PASSWORD
+posts = requests.get("https://api.npoint.io/5abcca6f4e39b4955965").json()
+post_objects = []
+for post in posts:
+    post_obj = Post(post["id"], post["title"], post["subtitle"], post["body"])
+    post_objects.append(post_obj)
 
-chrome_driver_path = YOUR CHROME DRIVER PATH
-driver = webdriver.Chrome(executable_path=chrome_driver_path)
+app = Flask(__name__)
 
-driver.get("http://www.tinder.com")
 
-sleep(2)
-login_button = driver.find_element_by_xpath('//*[@id="content"]/div/div[1]/div/main/div[1]/div/div/header/div[1]/div[2]/div/button')
-login_button.click()
+@app.route('/')
+def get_all_posts():
+    return render_template("index.html", all_posts=post_objects)
 
-sleep(2)
-fb_login = driver.find_element_by_xpath('//*[@id="modal-manager"]/div/div/div[1]/div/div[3]/span/div[2]/button')
-fb_login.click()
 
-sleep(2)
-base_window = driver.window_handles[0]
-fb_login_window = driver.window_handles[1]
-driver.switch_to.window(fb_login_window)
-print(driver.title)
+@app.route("/post/<int:index>")
+def show_post(index):
+    requested_post = None
+    for blog_post in post_objects:
+        if blog_post.id == index:
+            requested_post = blog_post
+    return render_template("post.html", post=requested_post)
 
-email = driver.find_element_by_xpath('//*[@id="email"]')
-password = driver.find_element_by_xpath('//*[@id="pass"]')
 
-email.send_keys(FB_EMAIL)
-password.send_keys(FB_PASSWORD)
-password.send_keys(Keys.ENTER)
+if __name__ == "__main__":
+    app.run(debug=True)
 
-driver.switch_to.window(base_window)
-print(driver.title)
 
-sleep(5)
-allow_location_button = driver.find_element_by_xpath('//*[@id="modal-manager"]/div/div/div/div/div[3]/button[1]')
-allow_location_button.click()
-notifications_button = driver.find_element_by_xpath('//*[@id="modal-manager"]/div/div/div/div/div[3]/button[2]')
-notifications_button.click()
-cookies = driver.find_element_by_xpath('//*[@id="content"]/div/div[2]/div/div/div[1]/button')
-cookies.click()
-
-for n in range(100):
-    sleep(1)
-    try:
-        print("called")
-        like_button = driver.find_element_by_xpath(
-            '//*[@id="content"]/div/div[1]/div/main/div[1]/div/div/div[1]/div/div[2]/div[4]/button')
-        like_button.click()
-    except ElementClickInterceptedException:
-        try:
-            match_popup = driver.find_element_by_css_selector(".itsAMatch a")
-            match_popup.click()
-        except NoSuchElementException:
-            sleep(2)
-
-driver.quit()
